27464ef8e3f1c17ad9888c6b5ece4d4bd8452930
Ganijon Madrimov <ganimac@ganijons-mbp.mynetworksettings.com>
2022-09-25 16:16:26 -0400

    Added template file and rendering

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import BlogPost
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    posts = BlogPost.objects.all()
+    return render(request, 'home.html', context={'posts':posts})
