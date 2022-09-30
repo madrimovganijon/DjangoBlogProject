5ff70564082588bbea4453bfa6cb7a80844e5bcc
Ganijon Madrimov <ganimac@ganijons-mbp.mynetworksettings.com>
2022-09-30 10:51:14 -0400

    Added newpost page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, newpost
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('newpost', newpost, name='newpost'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.forms import modelform_factory
 
 from .models import BlogPost
 
 # Create your views here.
 def home(request):
     posts = BlogPost.objects.all()
-    return render(request, 'home.html', context={'posts':posts})
+    return render(request, 'blog/home.html', context={'posts':posts})
+
+NewPostForm = modelform_factory(BlogPost, exclude=[])
+
+def newpost(request):
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = NewPostForm()
+    return render(request,'blog/newpost.html', context={'form':form})
