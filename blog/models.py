580d808d7ba0851072dfb0a09cd9ca1f31be9541
Ganijon Madrimov <ganimac@ganijons-mbp.mynetworksettings.com>
2022-10-01 14:57:01 -0400

    Added author and bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author}"
